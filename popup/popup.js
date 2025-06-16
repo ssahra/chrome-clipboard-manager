d086080fb460b5feddcb65b68fc2a288833db3e5
ssahra1 <66325674+ssahra@users.noreply.github.com>
2025-06-16 12:47:32 +0100

    copy and delete buttons added + copied text shortened

--- popup/popup.js
+++ popup/popup.js
@@ -11,10 +11,56 @@ document.addEventListener("DOMContentLoaded", () => {
   
     function renderHistory(items) {
       historyList.innerHTML = "";
-      items.forEach(item => {
+
+      
+
+      items.forEach((item, index) => {
         const li = document.createElement("li");
         //li.textContent = item; shows the item e.g. [object, Object]
-        li.textContent = item.text;
+        //li.textContent = item.text;
+
+        //const textSpan = document.createElement("span");
+        //const previewText = item.text.substring(0, 50);
+        //textSpan.textContent = previewText;
+
+        //if item.text > 50 characters: (const previewText = item.text.substring(0, 50);
+        //textSpan.textContent = previewText;)
+        // else (const textSpan = document.createElement("span");
+        //textSpan.textContent = item.text;)
+        const textSpan = document.createElement("span");
+        const copiedText = item.text;
+        if (copiedText.length > 50) {
+          const previewText = copiedText.substring(0, 50) + "...";
+          textSpan.textContent = previewText;
+        }
+        else {
+          textSpan.textContent = item.text;
+        }
+
+        // Copy button
+        const copyBtn = document.createElement("button");
+        copyBtn.textContent = "Copy";
+        copyBtn.addEventListener("click", () => {
+          navigator.clipboard.writeText(item.text);
+        });
+
+        // Delete button
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.addEventListener("click", () => {
+          console.log("Deleting item:", item.text);
+          console.log("At index:", index);
+
+          items.splice(index, 1); // remove from array
+          chrome.storage.local.set({ history: items }, () => {
+            renderHistory(items); // re-render list
+          });
+        });
+        
+
+        li.appendChild(textSpan);
+        li.appendChild(copyBtn);
+        li.appendChild(deleteBtn);
         historyList.appendChild(li);
       });
     }
